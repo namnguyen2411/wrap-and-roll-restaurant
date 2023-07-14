3b090bf2e4c7e059764140b8bc4de8942634a58e
NPN-PC <phuongnam.nguyen241196@gmail.com>
2023-07-14 18:18:45 +0700

    Create pages folder

---
+++ src/pages/index.ts
@@ -0,0 +1,7 @@
+export { default as Home } from './Home/Home'
+export { default as Menu } from './About/About'
+export { default as About } from './About/About'
+export { default as Contact } from './Contact/Contact'
+export { default as Booking } from './Booking/Booking'
+export { default as Register } from './Register/Register'
+export { default as Login } from './Login/Login'
