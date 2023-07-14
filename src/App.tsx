c39c06eee142a504650fa1e7c95dc61de2793ba1
NPN-PC <phuongnam.nguyen241196@gmail.com>
2023-07-14 20:46:18 +0700

    add routes for pages components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { Routes, Route } from 'react-router-dom'
+import { Contact, Booking, Home, About, Menu, Register, Login } from './pages'
+import { MainLayout } from './layouts'
+
 function App() {
-  return <h1 className='text-3xl font-bold underline text-red-400'>Hello world!</h1>
+  return (
+    <>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/menu' element={<Menu />} />
+          <Route path='/wrap-roll' element={<About />} />
+          <Route path='/booking' element={<Booking />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Route>
+      </Routes>
+    </>
+  )
 }
 
 export default App
